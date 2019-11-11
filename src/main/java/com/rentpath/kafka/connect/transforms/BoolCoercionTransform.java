12c6dbb0ea441db681001ecf268fe25f3db8181b
Jeremy Holland <jeremy@jeremypholland.com>
2019-11-11 10:53:15 -0600

    Bool Coercion and String Split SMTs
    [Card](https://rentpath.atlassian.net/browse/SRV-1179)
    
    This adds two new SMTs (Single Message Transforms) that are effectively the inversions of the original two.
    
    `BoolCoercionTransform` attempts to interpret a non-boolean source value as a boolean, e.g.:
    - String values "true", "yes", and "1" (case-insensitive) are interpreted as logical `true`
    - String values "false", "no", and "0" (case-insensitive) are interpreted as logical `false`
    - Integer value 0 is interpreted as logical `false`
    - Any other integer value is interpreted as logical `true`
    - Boolean values are unchanged
    Additionally, this transform has an optional flag to interpret a null source value as logical `false`.
    
    `StringSplitTransform` attempts to interpret a delimited-string source value as a list of strings, with options to interpret null and empty values appropriately (see config class `StringSplitTransformConfig` for details).

---
+++ src/main/java/com/rentpath/kafka/connect/transforms/BoolCoercionTransform.java
@@ -0,0 +1,121 @@
+package com.rentpath.kafka.connect.transforms;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class BoolCoercionTransform<R extends ConnectRecord<R>> implements Transformation<R> {
+    private static final Logger log = LoggerFactory.getLogger(BoolCoercionTransform.class);
+    public static final Pattern PATTERN_TRUE = Pattern.compile("(?i)^true$");
+    public static final Pattern PATTERN_FALSE = Pattern.compile("(?i)^false");
+    public static final Pattern PATTERN_ONE = Pattern.compile("^1$");
+    public static final Pattern PATTERN_ZERO = Pattern.compile("^0$");
+    public static final Pattern PATTERN_YES = Pattern.compile("^yes$");
+    public static final Pattern PATTERN_NO = Pattern.compile("^yes$");
+
+    private BoolCoercionTransformConfig config;
+
+    @Override
+    public R apply(R record) {
+        if (null == record.valueSchema() || Schema.Type.STRUCT != record.valueSchema().type()) {
+            log.trace("record.valueSchema() is null or record.valueSchema() is not a struct.");
+            return record;
+        }
+
+        Struct inputRecord = (Struct) record.value();
+        Schema inputSchema = inputRecord.schema();
+
+        final SchemaBuilder builder = SchemaBuilder.struct();
+        if (inputSchema.name() != null && !inputSchema.name().equals("")) {
+            builder.name(inputSchema.name());
+        }
+        if (inputSchema.isOptional()) {
+            builder.optional();
+        }
+        for (Field field : inputSchema.fields()) {
+            final Schema fieldSchema;
+            if (this.config.fields.contains(field.name())) {
+                fieldSchema = (field.schema().isOptional() || !config.coercionFalsifyNull || !config.coercionFalsifyEmpty) ?
+                        Schema.OPTIONAL_BOOLEAN_SCHEMA :
+                        Schema.BOOLEAN_SCHEMA;
+            } else {
+                fieldSchema = field.schema();
+            }
+            builder.field(field.name(), fieldSchema);
+        }
+        Schema schema = builder.build();
+        Struct struct = new Struct(schema);
+        for (Field field : inputSchema.fields()) {
+            if (this.config.fields.contains(field.name())) {
+                if (inputRecord.get(field.name()) == null) {
+                    if (config.coercionFalsifyNull)
+                        struct.put(field.name(), false);
+                    else
+                        struct.put(field.name(), null);
+                } else if (field.schema().type() == Schema.Type.STRING) {
+                    String v = inputRecord.getString(field.name());
+                    if (v.equals(""))
+                        if (config.coercionFalsifyEmpty)
+                            struct.put(field.name(), false);
+                        else
+                            struct.put(field.name(), null);
+                    else if (PATTERN_FALSE.matcher(v).matches() ||
+                            PATTERN_ZERO.matcher(v).matches() ||
+                            PATTERN_NO.matcher(v).matches())
+                        struct.put(field.name(), false);
+                    else if (PATTERN_TRUE.matcher(v).matches() ||
+                            PATTERN_ONE.matcher(v).matches() ||
+                            PATTERN_YES.matcher(v).matches())
+                        struct.put(field.name(), true);
+                    else
+                        throw new DataException(String.format("Field has non-boolean-interpretable string value: %s", v));
+                } else if (field.schema().type() == Schema.Type.INT8 ||
+                        field.schema().type() == Schema.Type.INT16 ||
+                        field.schema().type() == Schema.Type.INT32 ||
+                        field.schema().type() == Schema.Type.INT64) {
+                    long longValue = inputRecord.getInt64(field.name());
+                    if (longValue == 0)
+                        struct.put(field.name(), false);
+                    else
+                        struct.put(field.name(), true);
+                } else if (field.schema().type() == Schema.Type.BOOLEAN)
+                    struct.put(field.name(), inputRecord.getBoolean(field.name()));
+            } else {
+                struct.put(field.name(), inputRecord.get(field.name()));
+            }
+        }
+        return record.newRecord(
+                record.topic(),
+                record.kafkaPartition(),
+                record.keySchema(),
+                record.key(),
+                struct.schema(),
+                struct,
+                record.timestamp()
+        );
+    }
+
+    @Override
+    public ConfigDef config() {
+        return BoolStringTransformConfig.config();
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+        this.config = new BoolCoercionTransformConfig(map);
+    }
+}
---
+++ src/main/java/com/rentpath/kafka/connect/transforms/StringSplitTransformConfig.java
@@ -0,0 +1,53 @@
+package com.rentpath.kafka.connect.transforms;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class StringSplitTransformConfig extends AbstractConfig {
+    public static final String FIELDS_CONF = "fields";
+    static final String FIELDS_DOC = "The fields in the source record containing strings to split";
+
+    public static final String DELIMITER_CONF = "delimiter";
+    static final String DELIMITER_DOC = "The regular expression pattern to be used to split the string into a list";
+    static final String DELIMITER_DEFAULT = ",";
+
+    public static final String ELEMENT_NULLIFY_EMPTY_CONF = "element.nullify.empty";
+    static final String ELEMENT_NULLIFY_EMPTY_DOC = "Whether empty elements in the split list (e.g. the second value of \"1,,2\") ought to be interpreted as null. If false (default), will interpret as empty string";
+    static final boolean ELEMENT_NULLIFY_EMPTY_DEFAULT = false;
+
+    public static final String NULLIFY_EMPTY_CONF = "element.nullify.empty";
+    static final String NULLIFY_EMPTY_DOC = "Whether an empty string source value should be interpreted as null or an empty list (default)";
+    static final boolean NULLIFY_EMPTY_DEFAULT = false;
+
+    public static final String LISTIFY_NULL_CONF = "element.nullify.empty";
+    static final String LISTIFY_NULL_DOC = "Whether a null sourc value should be interpreted as an empty list or null (default)";
+    static final boolean LISTIFY_NULL_DEFAULT = false;
+
+    public final List<String> fields;
+    public final String delimiter;
+    public final boolean elementNullifyEmpty;
+    public final boolean nullifyEmpty;
+    public final boolean listifyNull;
+
+    public StringSplitTransformConfig(Map<String, ?> parsedConfig) {
+        super(config(), parsedConfig);
+        this.fields = getList(FIELDS_CONF);
+        this.delimiter = getString(DELIMITER_CONF);
+        this.elementNullifyEmpty = getBoolean(ELEMENT_NULLIFY_EMPTY_CONF);
+        this.nullifyEmpty = getBoolean(NULLIFY_EMPTY_CONF);
+        this.listifyNull = getBoolean(LISTIFY_NULL_CONF);
+    }
+
+    static ConfigDef config() {
+        return new ConfigDef()
+                .define(FIELDS_CONF, ConfigDef.Type.LIST, null, ConfigDef.Importance.HIGH, FIELDS_DOC)
+                .define(DELIMITER_CONF, ConfigDef.Type.STRING, DELIMITER_DEFAULT, ConfigDef.Importance.HIGH, DELIMITER_DOC)
+                .define(ELEMENT_NULLIFY_EMPTY_CONF, ConfigDef.Type.BOOLEAN, ELEMENT_NULLIFY_EMPTY_DEFAULT, ConfigDef.Importance.HIGH, ELEMENT_NULLIFY_EMPTY_DOC)
+                .define(NULLIFY_EMPTY_CONF, ConfigDef.Type.BOOLEAN, NULLIFY_EMPTY_DEFAULT, ConfigDef.Importance.HIGH, NULLIFY_EMPTY_DOC)
+                .define(LISTIFY_NULL_CONF, ConfigDef.Type.BOOLEAN, LISTIFY_NULL_DEFAULT, ConfigDef.Importance.HIGH, LISTIFY_NULL_DOC);
+    }
+}
