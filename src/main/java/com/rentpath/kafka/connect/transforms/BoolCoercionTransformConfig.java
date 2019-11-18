1913fd93fa0f5af03fa9f51da609935ee74a0b36
Jeremy Holland <jholland@rentpath.com>
2019-11-18 08:43:20 -0600

    Bool Coercion and String Split SMTs (#3)
    [Card](https://rentpath.atlassian.net/browse/SRV-1179)
    
    This adds two new SMTs (Single Message Transforms) that are effectively the inversions of the original two.
    
    `BoolCoercionTransform` attempts to interpret a non-boolean source value as a boolean, e.g.:
    - String values "true", "yes", and "1" (case-insensitive) are interpreted as logical `true`
    - String values "false", "no", and "0" (case-insensitive) are interpreted as logical `false`
    - Integer value 0 is interpreted as logical `false`
    - Any other integer value is interpreted as logical `true`
    - Boolean values are unchanged
    Additionally, this transform has an optional flag to interpret a null source value as logical `false`.
    
    `StringSplitTransform` attempts to interpret a delimited-string source value as a list of strings, with options to interpret null and empty values appropriately (see config class `StringSplitTransformConfig` for details).

---
+++ src/main/java/com/rentpath/kafka/connect/transforms/BoolCoercionTransformConfig.java
@@ -0,0 +1,39 @@
+package com.rentpath.kafka.connect.transforms;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
+import java.util.List;
+import java.util.Map;
+
+public class BoolCoercionTransformConfig extends AbstractConfig {
+    public static final String FIELDS_CONF = "fields";
+    static final String FIELDS_DOC = "The fields in the source record that contain booleans to coerce";
+
+    public static final String COERCION_FALSIFY_NULL_CONF = "coercion.falsify.null";
+    static final String COERCION_FALSIFY_NULL_DOC = "Will a null value be interpreted as logical false or null (default)?";
+    static final boolean COERCION_FALSIFY_NULL_DEFAULT = false;
+
+    public static final String COERCION_FALSIFY_EMPTY_CONF = "coercion.falsify.empty";
+    static final String COERCION_FALSIFY_EMPTY_DOC = "Will an empty string value be interpreted as logical false or null (default)?";
+    static final boolean COERCION_FALSIFY_EMPTY_DEFAULT = false;
+
+    public List<String> fields;
+    public boolean coercionFalsifyNull = false;
+    public boolean coercionFalsifyEmpty = false;
+
+    public BoolCoercionTransformConfig(Map<String, ?> parsedConfig) {
+        super(config(), parsedConfig);
+        this.fields = getList(FIELDS_CONF);
+        this.coercionFalsifyNull = getBoolean(COERCION_FALSIFY_NULL_CONF);
+        this.coercionFalsifyEmpty = getBoolean(COERCION_FALSIFY_EMPTY_CONF);
+    }
+
+    static ConfigDef config() {
+        return new ConfigDef()
+                .define(FIELDS_CONF, ConfigDef.Type.LIST, null, ConfigDef.Importance.HIGH, FIELDS_DOC)
+                .define(COERCION_FALSIFY_NULL_CONF, ConfigDef.Type.BOOLEAN, COERCION_FALSIFY_NULL_DEFAULT, ConfigDef.Importance.HIGH, COERCION_FALSIFY_NULL_DOC)
+                .define(COERCION_FALSIFY_EMPTY_CONF, ConfigDef.Type.BOOLEAN, COERCION_FALSIFY_EMPTY_DEFAULT, ConfigDef.Importance.HIGH, COERCION_FALSIFY_EMPTY_DOC);
+    }
+}
+
---
+++ src/main/java/com/rentpath/kafka/connect/transforms/StringSplitTransform.java
@@ -0,0 +1,101 @@
+package com.rentpath.kafka.connect.transforms;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class StringSplitTransform<R extends ConnectRecord<R>> implements Transformation<R> {
+    private static final Logger log = LoggerFactory.getLogger(StringSplitTransform.class);
+
+    private StringSplitTransformConfig config;
+
+    @Override
+    public R apply(R record) {
+        if (null == record.valueSchema() || Schema.Type.STRUCT != record.valueSchema().type()) {
+            log.trace("record.valueSchema() is null or record.valueSchema() is not a struct.");
+            return record;
+        }
+
+        Struct inputRecord = (Struct) record.value();
+        Schema inputSchema = inputRecord.schema();
+
+        final SchemaBuilder builder = SchemaBuilder.struct();
+        if (inputSchema.name() != null && !inputSchema.name().equals("")) {
+            builder.name(inputSchema.name());
+        }
+        if (inputSchema.isOptional()) {
+            builder.optional();
+        }
+        for (Field field : inputSchema.fields()) {
+            final Schema fieldSchema;
+            if (this.config.fields.contains(field.name())) {
+                SchemaBuilder listBuilder = SchemaBuilder.array(
+                        config.elementNullifyEmpty ? Schema.OPTIONAL_STRING_SCHEMA : Schema.STRING_SCHEMA
+                );
+                if (field.schema().isOptional())
+                    listBuilder.optional();
+                fieldSchema = listBuilder.build();
+            } else {
+                fieldSchema = field.schema();
+            }
+            builder.field(field.name(), fieldSchema);
+        }
+        Schema schema = builder.build();
+        Struct struct = new Struct(schema);
+        for (Field field : schema.fields()) {
+            if (this.config.fields.contains(field.name())) {
+                String v = inputRecord.getString(field.name());
+                List<String> list = null;
+                if ((v == null && config.listifyNull) || (v != null && v.equals("") && !config.nullifyEmpty))
+                    list = new ArrayList<>();
+                else if (v != null && !v.equals("")) {
+                    list = new ArrayList<>();
+                    String[] originalStrs = v.split(config.delimiter);
+                    for (String element : originalStrs)
+                        if (element.equals("") && config.elementNullifyEmpty)
+                            list.add(null);
+                        else
+                            list.add(element);
+                }
+                struct.put(field.name(), list);
+            } else {
+                struct.put(field.name(), inputRecord.get(field.name()));
+            }
+        }
+        return record.newRecord(
+                record.topic(),
+                record.kafkaPartition(),
+                record.keySchema(),
+                record.key(),
+                struct.schema(),
+                struct,
+                record.timestamp()
+        );
+    }
+
+    @Override
+    public ConfigDef config() {
+        return ListJoinTransformConfig.config();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+        this.config = new StringSplitTransformConfig(map);
+    }
+}
